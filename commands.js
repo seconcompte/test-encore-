21555d36a4320d2479441b5ca2f71e76a070f422
seconcompte <seconcompte1234@gmail.com>
2025-04-06 21:53:41 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,5 +1,5 @@
 // deploy-commands.js
-import { REST, Routes, ApplicationCommandOptionType, ApplicationCommandType } from 'discord.js';
+import { REST, Routes, ApplicationCommandOptionType } from 'discord.js';
 import 'dotenv/config';
 
 const commands = [
@@ -57,13 +57,23 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
   try {
-    console.log('Déploiement global des commandes slash en cours...');
+    // Déploiement sur le serveur de test d'abord
+    const testGuildId = "1287382398287216650";
+    console.log(`Déploiement des commandes sur le serveur ${testGuildId} en cours...`);
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, testGuildId),
+      { body: commands }
+    );
+    console.log(`Commandes déployées avec succès sur le serveur ${testGuildId}.`);
+
+    // Ensuite, déploiement global
+    console.log('Déploiement global des commandes en cours...');
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
     console.log('Les commandes slash globales ont été déployées avec succès.');
   } catch (error) {
-    console.error(error);
+    console.error('Erreur lors du déploiement des commandes slash:', error);
   }
 })();
